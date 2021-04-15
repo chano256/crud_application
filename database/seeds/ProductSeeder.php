6ab0cde5e4eca21826dac04a736eb471dbd52e07
Allan Ochan <allanochan@yahoo.com>
2021-04-15 10:55:23 +0300

    Added Product Seeder

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $products = [
+            ['name' => 'milk', 'price' => '299', 'description' => 'tall', 'service' => '0'],
+            ['name' => 'cod', 'price' => '1293', 'description' => 'long', 'service' => '1'],
+            ['name' => 'fish', 'price' => '1393', 'description' => 'medium', 'service' => '1'],
+        ];
+
+        foreach($products as $product) {
+            Product::create(array(
+                'name' => $product['name'], 
+                'price' => $product['price'],
+                'description' => $product['description'],
+                'service' => $product['service'],
+            ));
+        }
+    }
+}
